23f22e32a9a6bf3385efc0818e45c1de6257e376
Vinicius Bruscagini <vinibrenobr11@hotmail.com>
2023-01-26 23:07:22 -0300

    Finalizado formulário e criado visualização dos to dos

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -1,7 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DesafioBlazor.Models;
 
 public class Todo {
+
+    [Required]
+    [StringLength(30, ErrorMessage = "Identifier too long (16 character limit).")]
     public string Title { get; set; } = null!;
+
+    [Required]
     public string Text { get; set; } = null!;
+
     public string HexColor { get; set; } = null!;
 }
